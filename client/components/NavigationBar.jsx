8fb168b9c65046b0d8dcd1b42759099d54de5369
Ming Tung Wong <mingtungwong@outlook.com>
2017-12-01 23:22:44 -0500

    Minor styling changes

--- client/components/NavigationBar.jsx
+++ client/components/NavigationBar.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, PageHeader } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default class NavigationBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
+
     render() {
         return (
-            <Navbar style={{backgroundColor: "#178bd8"}}>
+            <Navbar style={{backgroundColor: "#45b5fa"}}>
                 <Navbar.Header>
                     <Navbar.Brand style={{paddingTop: "0"}}>
                         <Link to="/" className="navbar-left"><img src="/public/assets/logos/fgo_logo.png" height="63px" width="112px"/></Link>
                     </Navbar.Brand>
                 </Navbar.Header>
-                {/* <Nav>
-                    <NavItem eventKey={1} onClick={ e => this.props.history.push("/servants")}>Servants</NavItem>
-                </Nav> */}
+                <Nav>
+                    <LinkContainer to="/servants">
+                        <NavItem>Servants</NavItem>
+                    </LinkContainer>
+                </Nav>
             </Navbar>
         )
     }
