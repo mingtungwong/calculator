b6b2b6b2ac4f04ded5f6927f95e0d6614b0a2b09
Ming Tung Wong <mingtungwong@outlook.com>
2017-12-05 16:55:19 -0500

    Display item images for ascension materials on servant profile page

--- client/components/ServantProfile.jsx
+++ client/components/ServantProfile.jsx
@@ -1,14 +1,88 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class ServantProfile extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            servant: null
+        };
+    }
+
+    componentWillMount() {
+        axios.get(`http://localhost:1337/servant/cost/${this.props.match.params.id}`)
+        .then(res => res.data)
+        .then(servantObj => {
+            this.setState({servant: servantObj});
+        });
+    }
+
     render() {
 
-        console.log(this.props);
+        const servant = this.state.servant;
 
         return (
             <div>
-                This is a test
+                {
+                    this.state.servant ?
+                    <div>
+                        <div><h2>{servant.profile.name}</h2></div>
+                        <div>
+                        {
+                            new Array(servant.profile.stars)
+                                .fill(0)
+                                .map((x, idx) => <span key={idx} className="glyphicon glyphicon-star"></span>)
+                        }
+                        </div>
+                        <div>
+                            <img src={`/public/assets/servants/${'000'.slice(servant.profile.id.toString().length) + servant.profile.id}.png`}/>
+                        </div>
+                        <div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th className="t-asc-lvl">Ascension Level</th>
+                                        <th className="t-mat">1</th>
+                                        <th className="t-mat">2</th>
+                                        <th className="t-mat">3</th>
+                                        <th className="t-qp">4</th>
+                                        <th>QP</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        
+                                        servant.costs.map((level, idx) => {
+                                            console.log('in level', level);
+                                            return (
+
+                                                level ? 
+                                                <tr key={idx}>
+                                                    <td>{idx}</td>
+                                                    {
+                                                        level.map((item, index) => {
+                                                            console.log('in item', item);
+                                                            return (
+                                                                item ?
+                                                                item !== ' ' ?
+                                                                <td key={index}><img height="64px" width="64px" src={`/public/assets/items/${'000'.slice(item.id.toString().length) + item.id}.png`}/></td>
+                                                                : <td></td>
+                                                                : null
+                                                            );
+                                                        })
+                                                    }
+                                                </tr>
+                                                : null
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>  
+                        </div>
+                    </div>
+                    : null
+                }
             </div>
         )
     }
--- server/routes/servant.js
+++ server/routes/servant.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
 const db = require('../../db');
 
+router.get('/servant/:id', (req, res, next) => {
+    const query = `
+        SELECT servant.id, servant.name, servant_class.name AS class, servant.stars
+        FROM servant INNER JOIN servant_class ON servant.class_id = servant_class.id
+        WHERE servant.id = ${req.params.id}
+    `
+    db.query(query, (error, result) => {
+        if(error) next(error);
+        else if(!result) next(new Error("No results"));
+        else res.send(result.rows);
+    });
+});
+
 router.get('/servant', (req, res, next) => {
     const query = `
         SELECT servant.id, servant.name, servant_class.name AS class, servant.stars
@@ -8,21 +21,52 @@ router.get('/servant', (req, res, next) => {
         ORDER BY servant.id ASC
     `;
     db.query(query, (error, result) => {
+        if(error) next(error);
+        else if(!result) next(new Error("No results"));
         res.send(result.rows);
     })
-})
+});
 
 router.get('/servant/cost/:servant_id', (req, res, next) => {
-    const query = `
-        SELECT servant.name, ascension_costs.ascension_level, item.name AS item, ascension_costs.quantity
+    const servantQuery = `
+        SELECT servant.id, servant.name, servant_class.name AS class, servant.stars
+        FROM servant INNER JOIN servant_class ON servant.class_id = servant_class.id
+        WHERE servant.id = ${req.params.servant_id}
+    `;
+    const costQuery = `
+        SELECT servant.name, ascension_costs.ascension_level, item.id, item.name AS item, ascension_costs.quantity
         FROM servant INNER JOIN ascension_costs ON servant.id = ascension_costs.servant_id INNER JOIN item ON ascension_costs.item_id = item.id
         WHERE servant.id = ${req.params.servant_id}
     `;
-    db.query(query, (error, result) => {
+
+    const servantObject = {};
+
+    db.query(servantQuery, (error, result) => {
         if(error) next(error);
         else if(!result) next(new Error("No results"));
-        else res.send(result.rows);
+        else {
+            servantObject.profile = result.rows[0];
+            db.query(costQuery, (error, result) => {
+                if(error) next(error);
+                else if(!result) next(new Error("No results"));
+                else {
+                    const ascLevels = [];
+                    for(let row of result.rows) {
+                        if(!ascLevels[row.ascension_level]) ascLevels[row.ascension_level] = [];
+                        ascLevels[row.ascension_level].push({id: row.id, item: row.item, quantity: row.quantity});
+                    }
+                    for(let level of ascLevels) {
+                        if(level) {
+                            while(level.length < 5)
+                                level.push(' ');
+                        }
+                    }
+                    servantObject.costs = ascLevels;
+                    res.send(servantObject);
+                }
+            });
+        }
     });
-})
+});
 
 module.exports = router;
