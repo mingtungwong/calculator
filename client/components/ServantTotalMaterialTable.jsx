70fe10562f68f9e10dcc4116cd71a0de23c1dc3f
Ming Tung Wong <mingtungwong@outlook.com>
2017-12-29 14:25:27 -0500

    Fix errors

--- client/components/ServantTotalMaterialTable.jsx
+++ client/components/ServantTotalMaterialTable.jsx
@@ -5,21 +5,22 @@ export const ServantTotalMaterialTable = ({materials}) => (
     <div>
         <table>
             <thead>
-                <th>Material</th>
+                <tr>
+                    <th>Material</th>
+                </tr>
             </thead>
             <tbody>
                 {
-                    Object.keys(materials).map(key => {
-                    
-                        return (
-                            <tr>
+                    Object.keys(materials).map(key => 
+                        (
+                            <tr key={key}>
                                 <td className="mat-rows">
                                     <img height="64px" width="64px" src={`/public/assets/items/${pad(+key)}.png`}/>
                                     <p className="mat-quantity">{`x${materials[key]}`}</p>
                                 </td>
                             </tr>
                         )
-                    })
+                    )
                 }
             </tbody>
         </table>
