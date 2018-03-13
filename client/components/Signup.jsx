1ce74317221471078816875b08158b4a15797c97
Ming Tung Wong <mingtungwong@outlook.com>
2018-03-13 17:08:09 -0400

    Simple Sign up page layout

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form, FormGroup, FormControl, Col, ControlLabel, Button } from 'react-bootstrap';
 
 export default class Signup extends React.Component {
     constructor() {
@@ -7,9 +8,28 @@ export default class Signup extends React.Component {
 
     render() {
         return (
-            <div>
-                This is a test.
-            </div>
+            <Form horizontal>
+                <FormGroup controlId="signupHorizontalEmail">
+                    <Col sm={4} smOffset={4}>
+                        <FormControl type="email" placeholder="Email" />
+                    </Col>
+                </FormGroup>
+                <FormGroup controlId="signupHorizontalPassword">
+                    <Col sm={4} smOffset={4}>
+                        <FormControl type="password" placeholder="Password" />
+                    </Col>
+                </FormGroup>
+                <FormGroup controlId="signupHorizontalConfirmPassword">
+                    <Col sm={4} smOffset={4}>
+                        <FormControl type="password" placeholder="Confirm Password" />
+                    </Col>
+                </FormGroup>
+                <FormGroup>
+                    <Col sm={4} smOffset={4}>
+                        <Button type="submit">Submit</Button>
+                    </Col>
+                </FormGroup>
+            </Form>
         )
     }
 }
