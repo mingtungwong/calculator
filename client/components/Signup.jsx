48a5c65df202eb878dbb71dd52b1affcca73a2ec
Ming Tung Wong <mingtungwong@outlook.com>
2018-03-13 18:41:56 -0400

    Simple password validation UI

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -4,32 +4,56 @@ import { Form, FormGroup, FormControl, Col, ControlLabel, Button } from 'react-b
 export default class Signup extends React.Component {
     constructor() {
         super();
+        this.state = {
+            signupEmail: "",
+            signupPassword: "",
+            signupConfirmPassword: "",
+            passwordValidationState: null,
+            confirmPasswordValidationState: null   
+        }
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.validate = this.validate.bind(this);
     }
 
     handleSubmit(event) {
         const passwordValidation = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$");
-        const email = event.target.signupHorizontalEmail.value;
-        const password = event.target.signupHorizontalPassword.value;
-        const confirmPassword = event.target.signupHorizontalConfirmPassword.value;
-        console.log(email, password, confirmPassword);
-        console.log(passwordValidation.test(password));
+        const { signupEmail, signupPassword, signupConfirmPassword } = this.state;
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.id]: event.target.value}, () => {this.validate()});
+    }
+
+    validate() {
+        const passwordValidation = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$");
+        const passwordValidationState = passwordValidation.test(this.state.signupPassword) ? "success" : "error";
+        const confirmPasswordValidationState = this.state.signupPassword === this.state.signupConfirmPassword ? "success" : "error";
+        this.setState({ passwordValidationState, confirmPasswordValidationState });
     }
 
     render() {
         return (
             <Form horizontal onSubmit={this.handleSubmit}>
-                <FormGroup controlId="signupHorizontalEmail">
+                <FormGroup controlId="signupEmail" onChange={this.handleChange}>
                     <Col sm={4} smOffset={4}>
                         <FormControl type="email" placeholder="Email" minLength="1"/>
                     </Col>
                 </FormGroup>
-                <FormGroup controlId="signupHorizontalPassword">
+                <FormGroup
+                    controlId="signupPassword"
+                    onChange={this.handleChange}
+                    validationState={this.state.passwordValidationState}    
+                >
                     <Col sm={4} smOffset={4}>
                         <FormControl type="password" placeholder="Password"/>
                     </Col>
                 </FormGroup>
-                <FormGroup controlId="signupHorizontalConfirmPassword">
+                <FormGroup
+                    controlId="signupConfirmPassword"
+                    onChange={this.handleChange}
+                    validationState={this.state.confirmPasswordValidationState}
+                >
                     <Col sm={4} smOffset={4}>
                         <FormControl type="password" placeholder="Confirm Password"/>
                     </Col>
