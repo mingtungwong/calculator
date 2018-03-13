1b22197644d0b719c9a8ad0b2664783ae4ae2fd3
Ming Tung Wong <mingtungwong@outlook.com>
2018-03-13 17:56:26 -0400

    Simple tests for password validation

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -4,24 +4,34 @@ import { Form, FormGroup, FormControl, Col, ControlLabel, Button } from 'react-b
 export default class Signup extends React.Component {
     constructor() {
         super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        const passwordValidation = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$");
+        const email = event.target.signupHorizontalEmail.value;
+        const password = event.target.signupHorizontalPassword.value;
+        const confirmPassword = event.target.signupHorizontalConfirmPassword.value;
+        console.log(email, password, confirmPassword);
+        console.log(passwordValidation.test(password));
     }
 
     render() {
         return (
-            <Form horizontal>
+            <Form horizontal onSubmit={this.handleSubmit}>
                 <FormGroup controlId="signupHorizontalEmail">
                     <Col sm={4} smOffset={4}>
-                        <FormControl type="email" placeholder="Email" />
+                        <FormControl type="email" placeholder="Email" minLength="1"/>
                     </Col>
                 </FormGroup>
                 <FormGroup controlId="signupHorizontalPassword">
                     <Col sm={4} smOffset={4}>
-                        <FormControl type="password" placeholder="Password" />
+                        <FormControl type="password" placeholder="Password"/>
                     </Col>
                 </FormGroup>
                 <FormGroup controlId="signupHorizontalConfirmPassword">
                     <Col sm={4} smOffset={4}>
-                        <FormControl type="password" placeholder="Confirm Password" />
+                        <FormControl type="password" placeholder="Confirm Password"/>
                     </Col>
                 </FormGroup>
                 <FormGroup>
