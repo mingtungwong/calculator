5a892a284ca5afd11d570b342013026a33271ccb
Ming Tung Wong <mingtungwong@outlook.com>
2018-03-14 18:34:40 -0400

    Initial setup for authentication routes on the server

---
+++ server/routes/authentication.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const db = require('../../db');
+const utils = require('../utilities');
+
+/**
+ * POST routes
+ */
+
+ router.post('/signup', (req, res, next) => {
+    
+ })
+
+module.exports = router;
