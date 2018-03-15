c9afceafacd11a5afd615382e97b91408c9e19b7
Ming Tung Wong <mingtungwong@outlook.com>
2018-03-15 18:07:38 -0400

    Add more utility functions to server

--- server/utilities.js
+++ server/utilities.js
@@ -6,9 +6,27 @@ module.exports = {
         else if(!result) next(new Error("No result"));
         else res.send(result.rows);
     },
-    handleQuery: function(query, res, next) {
+    handleQuery: (query, res, next) => {
         db.query(query, (error, result) => {
             this.handleResponse(res, next, error, result);
         });
+    },
+    handleDBTransaction: (res, queryText, handleResultsFunction) => {
+        let error = false;
+        (async () => {
+            const client = await db.connect();
+            try {
+                await client.query('BEGIN');
+                const { rows } = await client.query(queryText);
+                await client.query('COMMIT');
+            } catch(e) {
+                await client.query('ROLLBACK');
+                error = true;
+                throw e;
+            } finally {
+                client.release();
+                handleResultsFunction(res, rows, error);
+            }
+        })();
     }
 }
