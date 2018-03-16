f117567fd3ceb42be1a96a6416edd4bd6d051929
Ming Tung Wong <mingtungwong@outlook.com>
2018-03-16 12:49:31 -0400

    Begin new post route for adding servant classes

--- server/routes/data.js
+++ server/routes/data.js
@@ -2,9 +2,21 @@ const router = require('express').Router();
 const db = require('../../db');
 const utils = require('../utilities');
 
+/**
+ * GET Routes
+ */
+
 router.get('/data/classes', (req, res, next) => {
     const query = `SELECT * FROM servant_class`;
     utils.handleQuery(query, res, next);
 });
+
+/**
+ * POST Routes
+ */
+
+ router.post('/data/add/class', (req, res, next) => {
+
+ });
 
 module.exports = router;
